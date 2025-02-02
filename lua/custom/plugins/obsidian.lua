75308586c1cf4751ba8a2c884a92bbfeb5013303
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-02-02 12:43:10 +0100

    add menu for obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -23,10 +23,37 @@ return {
     -- ft = "markdown",
     -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
     event = { unpack(events) },
+    cmd = {
+        "ObsidianMenu"
+    },
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
     opts = {
         workspaces = vaults,
     },
+    config = function(_, opts)
+        require("obsidian").setup(opts);
+
+        local actions = {
+            Search = 'ObsidianSearch',
+            Today = 'ObsidianToday',
+            Yesterday = 'ObsidianYesterday',
+            Tomorrow = 'ObsidianTomorrow',
+        }
+
+        vim.api.nvim_create_user_command('ObsidianMenu', function()
+            require('fzf-lua').fzf_exec(
+                vim.tbl_keys(actions),
+                {
+                    actions = {
+                        ["default"] = function(selected)
+                            vim.cmd(actions[selected[1]])
+                        end,
+                    },
+                    prompt = "Obsidian > ",
+                }
+            )
+        end, {})
+    end,
 }

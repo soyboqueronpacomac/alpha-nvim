20cc5e8dca9be09d9bdad76de164187a94667335
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-02-07 22:00:31 +0100

    update laravel config

--- lua/custom/plugins/laravel.lua
+++ lua/custom/plugins/laravel.lua
@@ -1,34 +1,49 @@
+local dir_path = vim.fn.expand("~/code/plugins/laravel.nvim")
+local dir_exists = vim.fn.isdirectory(dir_path) == 1
+
 return {
-    "adalessa/laravel.nvim",
-    enabled = require('nixCatsUtils').enableForCategory("laravel"),
-    dir = "~/code/plugins/laravel.nvim",
-    dependencies = {
-        "tpope/vim-dotenv",
-        "MunifTanjim/nui.nvim",
-        "nvim-lua/plenary.nvim",
-        "kevinhwang91/promise-async",
-    },
-    cmd = { "Laravel" },
-    keys = {
-        { "<leader>ll", "<cmd>Laravel<cr>" },
-        { "<c-g>",      "<cmd>Laravel view_finder<cr>" },
-        { "<leader>la", "<cmd>Laravel art<cr>" },
-        { "<leader>lr", "<cmd>Laravel routes<cr>" },
-        { "<leader>lh", "<cmd>Laravel art docs<cr>" },
-        { "<leader>lm", "<cmd>Laravel make<cr>" },
-        { "<leader>ln", "<cmd>Laravel related<cr>" },
-        { "<leader>lc", "<cmd>Laravel commands<cr>" },
-        { "<leader>lo", "<cmd>Laravel resources<cr>" },
-        { "<leader>lp", "<cmd>Laravel panel<cr>" },
-    },
-    event = { "VeryLazy" },
-    opts = {
-        lsp_server = "intelephense",
-        features = {
-            pickers = {
-                provider = "fzf-lua"
-            }
-        }
-    },
-    config = true,
+	"adalessa/laravel.nvim",
+	enabled = require("nixCatsUtils").enableForCategory("laravel"),
+	dir = dir_exists and dir_path or nil,
+	dependencies = {
+		"tpope/vim-dotenv",
+		"MunifTanjim/nui.nvim",
+		"nvim-lua/plenary.nvim",
+		"kevinhwang91/promise-async",
+	},
+	cmd = { "Laravel" },
+	keys = {
+		{ "<leader>ll", "<cmd>Laravel<cr>" },
+		{ "<c-g>", "<cmd>Laravel view_finder<cr>" },
+		{ "<leader>la", "<cmd>Laravel art<cr>" },
+		{ "<leader>lr", "<cmd>Laravel routes<cr>" },
+		{ "<leader>lh", "<cmd>Laravel art docs<cr>" },
+		{ "<leader>lm", "<cmd>Laravel make<cr>" },
+		{ "<leader>ln", "<cmd>Laravel related<cr>" },
+		{ "<leader>lc", "<cmd>Laravel commands<cr>" },
+		{ "<leader>lo", "<cmd>Laravel resources<cr>" },
+		{ "<leader>lp", "<cmd>Laravel panel<cr>" },
+		{
+			"gf",
+			function()
+				if require("laravel").app("gf").cursor_on_resource() then
+					return "<cmd>Laravel gf<CR>"
+				else
+					return "gf"
+				end
+			end,
+			noremap = false,
+			expr = true,
+		},
+	},
+	event = { "VeryLazy" },
+	opts = {
+		lsp_server = "intelephense",
+		features = {
+			pickers = {
+				provider = "snacks",
+			},
+		},
+	},
+	config = true,
 }

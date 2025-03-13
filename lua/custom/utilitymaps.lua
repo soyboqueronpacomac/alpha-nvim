28ccd27d2cd8b2fb51da502a0a3b39170f3bc5ac
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2025-03-13 09:20:46 +0100

    add maps to increase and decrease values

--- lua/custom/utilitymaps.lua
+++ lua/custom/utilitymaps.lua
@@ -3,3 +3,14 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent in and keeps the selection" })
 
 vim.keymap.set("n", "n", "nzzzv", { desc = "Goes to next result on the search and put the cursor in the middle" })
 vim.keymap.set("n", "N", "Nzzzv", { desc = "Goes to prev result on the search and put the cursor in the middle" })
+
+
+-- Remap Ctrl-Up to behave like Ctrl-a
+vim.api.nvim_set_keymap('n', '<C-Up>', '<C-a>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-Up>', '<C-a>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-Up>', '<C-a>', { noremap = true, silent = true })
+
+-- Remap Ctrl-Down to behave like Ctrl-x
+vim.api.nvim_set_keymap('n', '<C-Down>', '<C-x>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-Down>', '<C-x>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<C-Down>', '<C-x>', { noremap = true, silent = true })

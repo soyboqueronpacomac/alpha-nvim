8aa608b8db77586e9ce7ad96143b37b1b17f939e
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2025-05-07 08:35:35 +0200

    move from vim.select to snacks picker

--- lua/tools/make-runner.lua
+++ lua/tools/make-runner.lua
@@ -1,19 +1,32 @@
 vim.keymap.set({ "n" }, "<leader>mm", function()
-  -- TODO: should check if the file is redeable
-  local items =
-    vim.fn.systemlist("make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\\/\t=]*:([^=]|$)/ {split($1,A,/ /);print A[1]}'")
+	-- TODO: should check if the file is redeable
+	local items =
+		vim.fn.systemlist("make -qp | awk -F':' '/^[a-zA-Z0-9][^$#\\/\t=]*:([^=]|$)/ {split($1,A,/ /);print A[1]}'")
 
-  if vim.tbl_isempty(items) then
-    vim.notify("No make items", vim.log.levels.WARN, {})
-    return
-  end
+	if vim.tbl_isempty(items) then
+		vim.notify("No make items", vim.log.levels.WARN, {})
+		return
+	end
 
-  ---@diagnostic disable-next-line: param-type-mismatch
-  vim.ui.select(vim.fn.uniq(items), { promt = "Make command" }, function(choice)
-    if not choice then
-      return
-    end
-
-    vim.cmd(string.format("Dispatch make %s", choice))
-  end)
+	Snacks.picker.pick({
+		title = "Make Commands",
+		items = vim.iter(vim.fn.uniq(items))
+			:map(function(item)
+				return {
+					text = item,
+					value = item,
+				}
+			end)
+			:totable(),
+		layout = "vscode",
+		format = function(item, _)
+			return { { item.text } }
+		end,
+		confirm = function(picker, item)
+			picker:close()
+			if item then
+				vim.cmd(string.format("Dispatch make %s", item.value))
+			end
+		end,
+	})
 end, {})

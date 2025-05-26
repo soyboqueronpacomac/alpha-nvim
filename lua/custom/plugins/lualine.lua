e318835b643f0b97b09dd7b14d4f9f8bcace111d
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-05-26 22:03:10 +0200

    update the status line

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -5,69 +5,75 @@ if require("nixCatsUtils").enableForCategory("laravel") then
   y_section = {
     {
       function()
-        local ok, status = pcall(require("laravel").app, "status")
+        local ok, laravel_version = pcall(function ()
+          return Laravel.app("status"):get("laravel")
+        end)
         if ok then
-          return status:get("laravel")
+          return laravel_version
         end
       end,
       icon = { " ", color = { fg = "#F55247" } },
       cond = function()
-        local ok, status = pcall(require("laravel").app, "status")
-        if ok then
-          return require("laravel").app("status"):has("laravel")
-        end
-        return false
+        local ok, has_laravel_versions = pcall(function()
+          return Laravel.app("status"):has("laravel")
+        end)
+        return ok and has_laravel_versions
       end,
     },
     {
       function()
-        local ok, status = pcall(require("laravel").app, "status")
+        local ok, php_version = pcall(function()
+          return Laravel.app("status"):get("php")
+        end)
         if ok then
-          return status:get("php")
+          return php_version
         end
         return nil
       end,
       icon = { " ", color = { fg = "#AEB2D5" } },
       cond = function()
-        local ok, status = pcall(require("laravel").app, "status")
-        if ok then
-          return status:has("php")
-        end
-        return false
+        local ok, has_php_version = pcall(function()
+          return Laravel.app("status"):has("php")
+        end)
+        return ok and has_php_version
       end,
     },
     {
       function()
-        local ok, dev_command = pcall(require("laravel").app, "dev_command")
+        local ok, hostname = pcall(function()
+          return Laravel.app("dev_command"):hostname()
+        end)
         if ok then
-          return dev_command:hostname()
+          return hostname
         end
         return nil
       end,
       icon = { " ", color = { fg = "#8FBC8F" } },
       cond = function()
-        local ok, dev_command = pcall(require("laravel").app, "dev_command")
-        if ok then
-          return dev_command:isRunning()
-        end
-        return false
+        local ok, is_running = pcall(function()
+          return Laravel.app("dev_command"):isRunning()
+        end)
+        return ok and is_running
       end,
     },
     {
       function()
-        local ok, dump_server = pcall(require("laravel").app, "dump_server")
+        local ok, unseen_records = pcall(function()
+          return #(Laravel.app("dump_server"):unseenRecords())
+        end)
+
         if ok then
-          return #(dump_server:unseenRecords())
+          return unseen_records
         end
         return 0
       end,
       icon = { "󰱧 ", color = { fg = "#FFCC66" } },
       cond = function()
-        local ok, dump_server = pcall(require("laravel").app, "dump_server")
-        if ok then
-          return dump_server:isRunning()
-        end
-        return false
+        local ok, is_running = pcall(function()
+          return Laravel.app("dump_server"):isRunning()
+        end)
+
+        return ok and is_running
       end,
     },
   }

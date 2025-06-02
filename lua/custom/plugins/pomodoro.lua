115d7ae2e29af70ca2bfc6d18e55b8736696d8f8
Ariel D'Alessandro <ariel.dalessandro@auto1.com>
2025-06-02 09:06:52 +0200

    conditional pomodoro

--- lua/custom/plugins/pomodoro.lua
+++ lua/custom/plugins/pomodoro.lua
@@ -1,8 +1,9 @@
 local dir_path = vim.fn.expand("~/code/plugins/pomodoro")
+local dir_exists = vim.fn.isdirectory(dir_path) == 1
 
 return {
   "adalessa/pomodoro",
-  dir = dir_path,
+  dir = dir_exists and dir_path or nil,
   event = { "VeryLazy" },
   cmd = { "Pomodoro" },
   opts = {

71ff4207f1fc295cb3cdb01ff14437e06d8cdad9
Ariel D'Alessandro <dalessandro.ariel@gmail.com>
2025-06-21 12:45:27 +0200

    fix format, not sure why it changed

--- lsp/golangci_lint.lua
+++ lsp/golangci_lint.lua
@@ -3,7 +3,7 @@ return {
   cmd = { "golangci-lint-langserver" },
   filetypes = { "go" },
   init_options = {
-    command = { "golangci-lint", "run", "--out-format", "json", "--show-stats=false", "--issues-exit-code=1" },
+    command = { "golangci-lint", "run", "--output.json.path", "stdout", "--show-stats=false", "--issues-exit-code=1" },
   },
   root_markers = {
     "go.mod",
